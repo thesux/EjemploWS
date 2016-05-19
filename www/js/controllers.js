d2fe8ed949a5cbbe2360cdeb96618dff7dddd454
thesux <pop423@hotmail.com>
2016-05-19 16:27:48 -0500

    Beautify

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,38 +1,32 @@
 angular.module('starter.controllers', [])
 
-.controller('pruebaCtrl',function($scope,$timeout,ws)
-{
+.controller('pruebaCtrl', function($scope, $timeout, ws) {
 
 
-  $scope.onChange=function(id)
-  {
+  $scope.onChange = function(id) {
     console.log("Buscando estados");
-    ws.getEstados(id).then(function(arrEstados){
-      $scope.estados=arrEstados;
-      $scope.estadoSelect=$scope.estados[0];
-    },function(error){
+    ws.getEstados(id).then(function(arrEstados) {
+      $scope.estados = arrEstados;
+      $scope.estadoSelect = $scope.estados[0];
+    }, function(error) {
       console.log(error);
     });
 
   };
 
-  ws.getPaises().then(function(arrPaises)
-  {
+  ws.getPaises().then(function(arrPaises) {
     console.log(arrPaises);
-    $scope.paises=arrPaises;
-    $scope.paisSelect=$scope.paises[0];
+    $scope.paises = arrPaises;
+    $scope.paisSelect = $scope.paises[0];
 
-    ws.getEstados(1).then(function(arrEstados){
-      $scope.estados=arrEstados;
-      $scope.estadoSelect=$scope.estados[0];
-    },function(error){
+    ws.getEstados(1).then(function(arrEstados) {
+      $scope.estados = arrEstados;
+      $scope.estadoSelect = $scope.estados[0];
+    }, function(error) {
       console.log(error);
     });
   });
 
 
-
-
-  //console.log($scope.paises);
 
 })
--- www/js/services.js
+++ www/js/services.js
@@ -1,36 +1,82 @@
 angular.module('starter.services', [])
 
 
-.factory('ws',function($http,$q,$timeout){
+.factory('ws', function($http, $q, $timeout) {
 
-  var paises=[{id: 1,nombre: 'Mexico'},{id: 2,nombre:'Argentina'},{id: 3,nombre: 'USA'},{id: 4,nombre: 'España'},{id: 5, nombre: 'Japon'}];
-  var estados2=[[{id: 1,nombre: 'Estado1', idPais: 1},{id: 2,nombre: 'Estado2',idPais: 1}],
-                  [{id: 3,nombre: 'Estado3',idPais: 2},{id: 4,nombre: 'Estado4',idPais: 2}],
-                  [{id: 5,nombre: 'Estado5', idPais: 3},{id: 6,nombre: 'Estado6', idPais: 3}],
-                  [{id: 7,nombre: 'Estado7', idPais: 4}],
-                  [{id: 8,nombre: 'Estado8', idPais: 5}]];
+  var paises = [{
+    id: 1,
+    nombre: 'Mexico'
+  }, {
+    id: 2,
+    nombre: 'Argentina'
+  }, {
+    id: 3,
+    nombre: 'USA'
+  }, {
+    id: 4,
+    nombre: 'España'
+  }, {
+    id: 5,
+    nombre: 'Japon'
+  }];
+  var estados2 = [
+    [{
+      id: 1,
+      nombre: 'Estado1',
+      idPais: 1
+    }, {
+      id: 2,
+      nombre: 'Estado2',
+      idPais: 1
+    }],
+    [{
+      id: 3,
+      nombre: 'Estado3',
+      idPais: 2
+    }, {
+      id: 4,
+      nombre: 'Estado4',
+      idPais: 2
+    }],
+    [{
+      id: 5,
+      nombre: 'Estado5',
+      idPais: 3
+    }, {
+      id: 6,
+      nombre: 'Estado6',
+      idPais: 3
+    }],
+    [{
+      id: 7,
+      nombre: 'Estado7',
+      idPais: 4
+    }],
+    [{
+      id: 8,
+      nombre: 'Estado8',
+      idPais: 5
+    }]
+  ];
 
-  function getPaises()
-  {
-    var diferido=$q.defer();
-    $timeout(function(){
+  function getPaises() {
+    var diferido = $q.defer();
+    $timeout(function() {
       diferido.resolve(paises);
-    },2000);
+    }, 2000);
     return diferido.promise;
   }
 
-  function getEstados(id)
-  {
+  function getEstados(id) {
 
-    var diferido=$q.defer();
-    if(((id<1)||(id>5))||(id==null))
-    {
+    var diferido = $q.defer();
+    if (((id < 1) || (id > 5)) || (id == null)) {
       diferido.reject("Error: Pais no existente");
       return diferido.promise;
     }
-    $timeout(function(){
-      diferido.resolve(estados2[id-1]);
-    },2000);
+    $timeout(function() {
+      diferido.resolve(estados2[id - 1]);
+    }, 2000);
     return diferido.promise;
 
   }
